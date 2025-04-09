04d4fab4e0b3a0bfc28dbf6cbcd727bd2e2215e2
Amirborjas <amireduardo80@gmail.com>
2025-04-08 20:47:15 -0600

    Add files via upload

---
+++ ProductoExistenteException.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+public class ProductoExistenteException extends Exception {
+    public ProductoExistenteException(String mensaje) {
+        super(mensaje);
+    }
+}
